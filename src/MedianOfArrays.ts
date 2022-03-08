03ce99f1e7bebcad7673949886cf2a3e64963251
예슬 <43533905+2yeseul@users.noreply.github.com>
2022-03-08 22:41:07 +0900

    [solved] Q4

---
+++ src/MedianOfArrays.ts
@@ -0,0 +1,16 @@
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    const sortArrays = (a: number, b: number) => {
+        return a - b;
+    }
+    
+    const mergedNumbers = [...nums1, ...nums2].sort(sortArrays);
+    
+    console.log(mergedNumbers)
+    
+    const pivot = Math.trunc(mergedNumbers.length / 2);
+    
+    if (mergedNumbers.length % 2 === 0) {
+        return (mergedNumbers[(pivot - 1)] + mergedNumbers[pivot]) / 2
+    }
+    return mergedNumbers[pivot];
+};
